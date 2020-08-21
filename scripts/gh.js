15f560b991a35a3ada53b79517592c1e989748a1
FengFan <mail@fengfan.me>
2020-08-21 19:26:50 +0800

    build: add gh script

--- scripts/gh.js
+++ scripts/gh.js
@@ -6,8 +6,11 @@ run()
 
 async function run() {
     const pkgRoot = getPkgRoot('timecat')
-    const distDir = path.resolve(pkgRoot, 'dist')
+    const name = 'timecat.global.js'
+    const pkgDir = path.resolve(pkgRoot, 'dist')
+    const source = path.resolve(pkgDir, name)
+    const target = path.resolve(__dirname, '../docs/' + name)
     fs.removeSync(path.resolve(__dirname, '../docs'))
-    fs.copySync(distDir, path.resolve(__dirname, '../docs'))
+    fs.copySync(source, target)
     fs.copySync(path.resolve(__dirname, '../examples'), path.resolve(__dirname, '../docs'))
 }
